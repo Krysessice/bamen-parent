27b07f6e13338b795db91fc7c834b4d18001aba8
Administrator <123@163.com>
2017-08-18 15:38:07 +0800

    房卡模式的系统信息加载到redis。

---
+++ bamen-web/room-card-mode/src/main/java/com/bamenyouxi/room_card_mode/model/mysql/room_card_mode/SystemInfo.java
@@ -0,0 +1,53 @@
+package com.bamenyouxi.room_card_mode.model.mysql.room_card_mode;
+
+import com.bamenyouxi.core.impl.model.mysql.BaseEntity;
+
+public final class SystemInfo  extends BaseEntity{
+
+
+    private String  gameName;
+    private String title;
+    private Boolean _sysFlag;
+
+
+    public SystemInfo(){}
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Boolean get_sysFlag() {
+        return _sysFlag;
+    }
+
+
+    private SystemInfo(Builder builder) {
+        this.sysFlag = builder.sysFlag;
+        this._sysFlag = builder._sysFlag;
+    }
+
+    public static class Builder {
+        private Boolean sysFlag;
+        private Boolean _sysFlag;
+
+        public Builder sysFlag(Boolean val) {
+            sysFlag = val;
+            return this;
+        }
+
+        public Builder _sysFlag(Boolean val) {
+            _sysFlag = val;
+            return this;
+        }
+
+        public SystemInfo build() {
+            return new SystemInfo(this);
+        }
+    }
+
+
+}
---
+++ bamen-web/room-card-mode/src/main/java/com/bamenyouxi/room_card_mode/util/RedisUtil.java
@@ -0,0 +1,40 @@
+package com.bamenyouxi.room_card_mode.util;
+
+
+import com.bamenyouxi.core.constant.RedisConstant;
+import com.bamenyouxi.core.util.AbstractRedisUtil;
+import com.bamenyouxi.room_card_mode.model.mysql.room_card_mode.SystemInfo;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public  final  class RedisUtil  extends AbstractRedisUtil {
+
+
+    /**
+     * 获取存储系统信息的数据库引擎
+     * @return
+     */
+    public RedisTemplate<String , SystemInfo> getSystemInfoDB() {
+        return super.getInstanceN(RedisConstant.DataBase.SYSTEM_INFO_DB);
+    }
+
+
+    /**
+     * 存储 更新系统数据
+     * @param systemInfo
+     */
+    public void SaveSystemInfo(SystemInfo systemInfo){
+         getSystemInfoDB().opsForValue().set(SystemInfo.class.getName(),systemInfo);
+    }
+
+
+    /**
+     * 从redis中获取数据
+     * @return
+     */
+    public SystemInfo getSystemInfo(){return getSystemInfoDB().opsForValue().get(SystemInfo.class.getName());}
+
+
+}
