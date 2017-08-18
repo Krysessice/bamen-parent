b68b30f304b3c3b4c0a8395876f0630226c21b95
IceFruit <13477035189@163.com>
2017-08-18 10:53:54 +0800

    房卡模式sql。

---
+++ bamen-web/room-card-mode/src/main/resources/room_card_mode.sql
@@ -0,0 +1,38 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_mysql
+Source Server Version : 50718
+Source Host           : localhost:3306
+Source Database       : room_card_mode
+
+Target Server Type    : MYSQL
+Target Server Version : 50718
+File Encoding         : 65001
+
+Date: 2017-08-18 10:53:22
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for t_sys_agent
+-- ----------------------------
+DROP TABLE IF EXISTS `t_sys_agent`;
+CREATE TABLE `t_sys_agent` (
+  `F_ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `F_ACCOUNT` char(11) NOT NULL COMMENT '账号',
+  `F_NICK_NAME` varchar(50) NOT NULL COMMENT '昵称',
+  `F_PASSWORD` varchar(32) NOT NULL,
+  `F_SECRET_KEY` varchar(36) NOT NULL,
+  `F_SUPER_AGENT_ID` bigint(20) unsigned DEFAULT NULL COMMENT '上级id，主键id',
+  `F_FIRST_CHARGE_TIME` datetime DEFAULT NULL COMMENT '首充时间',
+  `F_LAST_LOGIN_TIME` timestamp NULL DEFAULT NULL COMMENT '上次登录时间',
+  `F_SYS_FLAG` bit(1) NOT NULL DEFAULT b'1' COMMENT '标识，1为可用，0为禁用',
+  `F_CREATOR` bigint(20) unsigned DEFAULT NULL COMMENT '创建者',
+  `F_CREATE_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `F_MODIFIER` bigint(20) unsigned DEFAULT NULL COMMENT '修改者',
+  `F_MODIFY_TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`F_ID`),
+  UNIQUE KEY `idx_sys_agent_unique1` (`F_ACCOUNT`) USING BTREE COMMENT '账号唯一'
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='后台代理';
